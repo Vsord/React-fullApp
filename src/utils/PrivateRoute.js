9304e8fb4ac55f8aaf87783ee9f71b346a512bd5
Vsord <stanislav060977@gmail.com>
2022-09-07 15:47:28 +0300

    SU-4311:routing bug fixed

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoute = () => {
-  let authPermission = localStorage.getItem("isAuthorized");
+  let authPermission = JSON.parse(localStorage.getItem("isAuthorized"));
   return authPermission ? <Outlet /> : <Navigate to="/login" />;
 };
 
